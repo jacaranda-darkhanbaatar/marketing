1dc982e90bfe24c3a513c160a6a59a0d72d3ded7
khashaa-jacaranda <151612157+khashaa-jacaranda@users.noreply.github.com>
2024-02-23 18:00:33 +0800

    You are my special

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,36 +2,33 @@ import { DiApple, DiAndroid } from "react-icons/di";
 
 const Header = () => {
   return (
-    <header id="header" className="bg-gradient-to-b from-sky-200 py-28 md:pt-36 xl:pt-44">
-      <div className="flex flex-between items-center">
-        <div className="ml-40">
-          <h1 className="font-bold text-6xl mb-7 text-balance">
+    <header
+      id="header"
+      className="bg-gradient-to-b from-sky-200 py-28 md:pt-36 xl:pt-44"
+    >
+      <div className="mx-auto px-4 sm:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+        <div className="mb-16 lg:mt-32 xl:mt-40 xl:mr-12 ml-40">
+          <h1 className="font-bold text-6xl mb-5 text-balance">
             Team management mobile application
           </h1>
-          <p className="text-slate-500 text-2xl text-balance mb-7">
+          <p className="text-slate-500 text-2xl text-balance mb-8">
             Start getting things done together with your team based on Pavo's
             revolutionary team management features
           </p>
-          <a
-            className="inline-block leading-none border border-transparent bg-indigo-600 rounded-full text-white font-bold px-10 py-4 mr-4 hover:border-indigo-600 hover:bg-transparent hover:text-indigo-600 duration-300"
-            href="#download-ios"
-          >
+          <button className="btn btn-primary mr-4 px-10 py-4"> 
             <DiApple size={25} className="inline-block mr-1 mb-1" />
             Download
-          </a>
-          <a
-            className="inline-block leading-none border border-transparent bg-pink-500 rounded-full text-white font-bold px-10 py-4 hover:border-pink-500 hover:bg-transparent hover:text-pink-500 duration-300"
-            href="#download-android"
-          >
+          </button>
+          <button className="btn btn-secondary px-10 py-4">
             <DiAndroid size={25} className="inline-block mr-1 mb-1" />
             Download
-          </a>
+          </button>
         </div>
-        <div>
+        <div className="xl:text-right">
           <img
             src="images/header-smartphone.png"
             alt="alternative"
-            className="mr-72"
+            className="mr-72 md-hidden"
           />
         </div>
       </div>
