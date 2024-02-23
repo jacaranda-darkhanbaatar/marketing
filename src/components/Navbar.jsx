d5401836840a908385ee940d5f10b318e84da01a
khashaa-jacaranda <151612157+khashaa-jacaranda@users.noreply.github.com>
2024-02-23 18:01:11 +0800

    You are my special

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { HiBars3 } from "react-icons/hi2";
+
 const Navbar = () => {
   const menus = [
     {
@@ -28,8 +30,8 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed w-full top-0 right-0 left-0 px-0">
-      <div className="flex justify-between">
+    <nav className="fixed w-full top-0 right-0 left-0 px-0 z-10">
+      <div className="flex flex-wrap justify-between items-center sm:px-4 lg:px-8 lg:flex-nowrap">
         <a href="index.html">
           <img
             src="images/logo.svg"
@@ -37,8 +39,11 @@ const Navbar = () => {
             className="w-40 h-30 ml-40 mt-10"
           />
         </a>
-        <div>
-          <ul className="flex flex-row font-semibold mr-40 mt-10">
+        <button className="bg-transparent rounded text-xl leading-none lg:hidden lg:text-gray-400" type="button">
+          <span className="inline-block w-8 h-8 align-middle"><HiBars3 className="h-8 w-8"/></span>
+        </button>
+        <div className="lg:static lg:w-auto lg:px-0 lg:bg-transparent lg:overflow-y-visible lg:visible hidden md:block sm:hidden">
+          <ul className="lg:flex lg:flex-row font-semibold mr-40 mt-10">
             {menus.map((menu) => (
               <li className="mx-3 first:text-pink-500 text-slate-500 hover:first:text-slate-500 hover:text-pink-500 duration-300">
                 <a href={`#${menu.link}`}>{menu.text}</a>
@@ -53,3 +58,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
